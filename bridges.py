beb0145e40bf59079652c2ea4b2fb27f24ed13d4
Genevieve Johnson <genrjohnson@gmail.com>
2017-03-06 21:55:02 -0600

    reduce values after un-twisting

--- bridges.py
+++ bridges.py
@@ -1,36 +1,46 @@
 #!/usr/bin/env python2.7
 
-def remove_tuple(tuple, knot):
-  print 'the tuple to be removed from the knot ' + str(knot) + ' is ' + str(tuple)
+# If two elements in the tuple are equal (with value x)
+  # Delete the tuple
+  # Decrease all elements with value greater than x
+  # print "The tuple [] has been eliminated"
+# return knot  
 
-def has_twist(tuple):
-  duplicate_value = has_duplicate_value(tuple)
+def remove_tuple(crossing, knot):  
+  knot.remove(crossing)
+  return knot
+
+def alter_elements_greater_than(knot, value, increment):
+  for place, crossing in enumerate(knot):
+    for index, element in enumerate(crossing):
+      if element > value:
+        crossing[index] = element + increment
+  return knot
+
+def has_twist(crossing):
+  duplicate_value = has_duplicate_value(crossing)
   if duplicate_value:
     return True
   else:
     return False
 
-    # If two elements in the tuple are equal (with value x)
-      # Delete the tuple
-      # Decrease all elements with value greater than x
-      # print "The tuple [] has been eliminated"
-  # return knot  
-
-def has_duplicate_value(tuple):
+def has_duplicate_value(crossing):
   sets = reduce(
     lambda (u, d), o : (u.union([o]), d.union(u.intersection([o]))),
-    tuple,
+    crossing,
     (set(), set()))
 
   if sets[1]:
-    return sets[1]
+    return list(sets[1])[0]
   else:
     return False
 
 def simplify_all_rm1(knot):
-  for tuple in knot:
-    if has_duplicate_value(tuple):
-      remove_tuple(tuple, knot)
+  for crossing in knot:
+    duplicate_value = has_duplicate_value(crossing)
+    if duplicate_value:
+      remove_tuple(crossing, knot)
+      alter_elements_greater_than(knot, duplicate_value, -2)
   return knot
 
 # Read in a CSV.
