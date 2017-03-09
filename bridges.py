44ddea35a0cd369b8fbcae73e29358a944fe7e3e
Genevieve Johnson <genrjohnson@gmail.com>
2017-03-08 19:30:58 -0600

    add tests

--- bridges.py
+++ bridges.py
@@ -6,6 +6,9 @@ import csv
 def remove_tuples(indices, knot):
     """Remove tuples from the PD notation of a knot.
 
+    >>> remove_tuples([1,3], [[4, 2, 5, 1], [8, 6, 1, 5], [6, 3, 7, 4], [2, 7, 3, 8]])
+    [[4, 2, 5, 1], [6, 3, 7, 4]]
+
     Arguments:
     indices -- the list of indices of the tuples to remove
     knot -- the knot from which to remove tuples
@@ -21,6 +24,11 @@ def alter_elements_greater_than(knot, value, increment):
     """Change the value of elements in the PD notation of a knot
     which are greater than some value.
 
+    >>> alter_elements_greater_than([[4, 2, 5, 1], [8, 6, 1, 5], [6, 3, 7, 4], [2, 7, 3, 8]], 4, -2)
+    [[4, 2, 3, 1], [6, 4, 1, 3], [4, 3, 5, 4], [2, 5, 3, 6]]
+    >>> alter_elements_greater_than([[4, 2, 5, 1], [8, 6, 1, 5], [6, 3, 7, 4], [2, 7, 3, 8]], 4, 2)
+    [[4, 2, 7, 1], [10, 8, 1, 7], [8, 3, 9, 4], [2, 9, 3, 10]]
+
     Arguments:
     knot -- the knot in which to alter elements
     value -- the integer against which to compare each element
@@ -34,6 +42,11 @@ def alter_elements_greater_than(knot, value, increment):
 
 def has_duplicate_value(crossing):
     """Find duplicate values in the PD notation of a crossing.
+
+    >>> has_duplicate_value([1, 2, 3, 4])
+    False
+    >>> has_duplicate_value([1, 2, 3, 3])
+    3
 
     Arguments:
     crossing -- the PD notation of a crossing
@@ -104,3 +117,7 @@ with open('knots.csv') as csvfile:
         print 'the original knot is ' + str(knot)
         simplify_rm1_recursive(knot)
         print 'the simplified knot is ' + str(knot)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
