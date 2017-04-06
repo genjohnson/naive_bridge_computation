496f9a2b01c8d9d52b00e973b2e7bd0a02e368d9
Genevieve Johnson <genrjohnson@gmail.com>
2017-04-06 12:55:08 -0500

    unittest for crossing.alter_elements_greater_than()

--- test_reduce_bridges.py
+++ test_reduce_bridges.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python2.7
 
-import itertools, reduce_bridges, unittest
+import itertools, unittest
 from reduce_bridges import *
+
+# Tests for Crossing methods.
+class AlterElementsGreaterThanTestCase(unittest.TestCase):
+  def testPositiveAddend(self):
+    crossing = Crossing([1,2,3,4], 0)
+    crossing.alter_elements_greater_than(2, 1)
+    self.assertEqual(crossing.pd_code, [1,2,4,5])
+
+  def testNegativeAddend(self):
+    crossing = Crossing([1,2,3,4], 0)
+    crossing.alter_elements_greater_than(2, -1)
+    self.assertEqual(crossing, Crossing([1,2,2,3],0))
 
 class HasDuplicateValueTestCase(unittest.TestCase):
   def testHasDuplicateValue(self):
