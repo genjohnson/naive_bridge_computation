9d3ed493282cd87e8af4b33d263aa5188cf8d26c
Genevieve Johnson <genrjohnson@gmail.com>
2017-04-10 22:40:59 -0500

    simplify_rm2_recursively() method and test

--- reduce_bridges.py
+++ reduce_bridges.py
@@ -154,6 +154,18 @@ class Knot:
             crossing.alter_elements_greater_than(max(segments_to_eliminate)-2, -2)
         return self
 
+    def simplify_rm2_recursively(self):
+        """Simplify a knot by Reidemeister moves of type 2 until
+        no more moves are possible.
+        """
+        while True:
+            moves_possible = self.has_rm2()
+            if moves_possible:
+                self.simplify_rm2(moves_possible[0], moves_possible[1])
+            if not moves_possible:
+                break;
+        return self
+
 def simplify_rm1_rm2_recursively(knot):
     """
     Simplify a knot by Reidemeister moves of types 1 & 2 until
