46d8bd5b6ae89a854e38a6ed82f90214809adbcb
Genevieve Johnson <genrjohnson@gmail.com>
2017-04-27 19:17:25 -0500

    add spaces

--- program.py
+++ program.py
@@ -17,13 +17,25 @@ with open('knots.csv') as csvfile:
     for row in knotreader:
         # Create a knot object.
         knot = create_knot_from_pd_code(ast.literal_eval(row['pd_notation']), row['name'])
-        # Simplify the knot.
+        # Simplify the knot now to avoid choosing bridges which will be
+        # discarded during simplification.
         knot.simplify_rm1_rm2_recursively()
+        # Pick initial bridges.
+        if (knot.num_crossings() > 0):
+            # Pick the first crossing as a bridge.
+            knot.designate_bridge(knot.crossings[0])
+            knot.extend_bridge(0)
+
+            # Pick the crossing which forms a "T" with the first bridge.
+
+
+            print str(knot.bridges)
+
         # Add the results to our output.
         knot_output['knots'].append(knot.json())
         # Output a message that the knot has been processed.
         print 'processed ' + knot.name
 
 # Write the results to our output JSON file.
-with open('output.json', mode='w') as outfile:
-    json.dump(knot_output, outfile, indent=2, cls=ComplexEncoder)
+with open('output.json', mode = 'w') as outfile:
+    json.dump(knot_output, outfile, indent = 2, cls = ComplexEncoder)
